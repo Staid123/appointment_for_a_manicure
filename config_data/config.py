e51231cc31f49932d8f59a7092e0e741eea58a64
Staid123 <pavlenkokirill568@gmail.com>
2023-08-10 18:35:30 +0300

    Файл конфигурации создан

---
+++ config_data/config.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class Database:
+    db_name: str    # Название файла где хранится база данных
+
+
+@dataclass
+class TgBot:
+    token: str        # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: Database
+
+
+# Функция для отправки конфига
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
+                               admin_ids=list(map(int, env.list('ADMIN_IDS')))),
+                  db=Database(db_name=env('DATABASE')))
+
+print(load_config())
