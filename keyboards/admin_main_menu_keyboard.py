5474d782127a2684ad0994ccbaefb542f101e92b
Staid123 <pavlenkokirill568@gmail.com>
2023-08-19 18:10:28 +0300

    Добавлен файл для основного меню админа

---
+++ keyboards/admin_main_menu_keyboard.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+from lexicon.lexicon_ru import LEXICON_admin_commands
+
+
+# Функция для настройки кнопки Menu бота
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [BotCommand(
+        command=command,
+        description=description
+    ) for command, description in LEXICON_admin_commands.items()]
+    await bot.set_my_commands(main_menu_commands)
