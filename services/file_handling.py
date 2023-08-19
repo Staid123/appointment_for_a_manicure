743405e2c97e72ecb2628afacae2edcec141ffb1
Staid123 <pavlenkokirill568@gmail.com>
2023-08-19 18:06:28 +0300

    Добавлены функции для сортировки текста

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,6 +1,13 @@
 import calendar
 
 
+def check_phone_number(phone_number) -> bool:
+    if phone_number.isdigit() and len(phone_number) == 10 and str(phone_number)[0] == '0':
+        return True
+    return False
+
+
+
 def day_in_months(year: str, month: int) -> list:
     result = []
     zero_days_calendar = calendar.Calendar().itermonthdays(int(year), int(month))
@@ -13,8 +20,19 @@ def day_in_months(year: str, month: int) -> list:
 
 
 def working_time_in_day() -> list:
-    lst = ['8:00', '10:00', '12:00', '14:00', '16:00', '18:00']
+    time = '8:00'
+    lst: list = [time]
+    while time != '18:00':
+        if str(int(time[-2:]) + 15) != '60':
+            hour = time.split(':')[0]
+            minutes = str(int(time[-2:]) + 15)
+        else:
+            hour = str(int(time.split(':')[0]) + 1)
+            minutes = '00'
+        time = hour + ':' + minutes
+        lst.append(time)
     return lst
+
 
 
 def format_func1(dct: dict) -> str:
@@ -24,14 +42,13 @@ def format_func1(dct: dict) -> str:
     return res
 
 
-
 def format_func2(lst: list, months: dict) -> str:
-    res = 'Вы работаете:\n\n'
+    res = 'К вам записаны:\n\n\n'
     for tupl in lst:
-        month = months[tupl[1]].lower()
+        month = months[tupl[3]].lower()
         if month[-1] in ['ь', 'й']:
             month = month.replace(month[-1], 'я')
         else:
             month += 'а'
-        res += f'{str(tupl[2])} {month} {str(tupl[0])} в {tupl[-1]}\n'
+        res += f'{tupl[0]} на {tupl[-1]} {tupl[4]} {month} {tupl[2]} года.\nНомер телефона клиента: {tupl[1]}\n\n'
     return res
