81211b59e95c0b2b0d41e0ce73984ab4f251a3fb
Staid123 <pavlenkokirill568@gmail.com>
2023-08-22 17:45:09 +0300

    mini upgrade

--- filters/admin_filter.py
+++ filters/admin_filter.py
@@ -1,13 +1,15 @@
-from config_data.config import load_config
+from config_data.config import load_config, Config
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
+from config_data.config import load_config
 
-
-ADMIN_IDS = 664760823, 2037720596, 5202602677
-
+config: Config = load_config()
 
 class IsAdmin(BaseFilter):
+    def __init__(self):
+        self.ADMIN_IDS = config.tg_bot.admin_ids
+
     async def __call__(self, message: Message) -> bool:
-        if message.from_user.id in ADMIN_IDS:
+        if message.from_user.id in self.ADMIN_IDS:
             return True
         return False
