c12b1d3177e0e4ab1824c403adad47c1370c2316
Staid123 <pavlenkokirill568@gmail.com>
2023-08-22 22:24:22 +0300

    Added new handler

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,10 +1,10 @@
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from aiogram import Router, F
-from lexicon.lexicon_ru import LEXICON_user, LEXICON_admin
-from services.file_handling import check_phone_number
+from lexicon.lexicon_ru import LEXICON_user, LEXICON_admin, months
+from services.file_handling import check_phone_number, format_func3
 from database.tatiana_working import get_some_records
-from database.client_database import AddRecord, CountRecords
+from database.client_database import AddRecord, CountRecords, GetRecords
 
 
 
@@ -34,13 +34,15 @@ async def enter_phone_number(message: Message):
             client_name: str = record[0]
             type_of_service: str = record[5]
             date: str = f'{str(record[1])}.{str(record[2])}.{str(record[3])}.{record[4]}'
+            # Создаем экземпляр класса CountRecords
+            ex = CountRecords(client_name, type_of_service, date, message.text, message.from_user.id)
             # Проверяем нету ли пользователя уже в базе данных
-            records = CountRecords(client_name, type_of_service, date, message.text, message.from_user.id)
-            count_records = records.count_records()
-            # Если нету то записываем в базу данных
+            count_records = ex.count_records()
+            # Если нету то записываем в базу данныx
             if not count_records:
-                # Записываем их в базу
+                # Создаем экземпляр класса
                 client =  AddRecord(client_name, type_of_service, date, message.text, message.from_user.id)
+                # Записываем их в базу
                 client.add_record()
             # Благодарим пользователя и предлагаем ему посмотреть на когда он записан
             # командой /see_my_records
@@ -49,3 +51,22 @@ async def enter_phone_number(message: Message):
         await message.answer(text=LEXICON_admin['bad_phone_number'])
     else:
         await message.answer(text=LEXICON_user['not recorded'])
+
+
+# Этот хэндлер будет срабатывать на ввод команды see_my_records
+@router.message(Command(commands=['see_my_records']))
+async def command_see_my_records(message: Message):
+    # Получаем экземпляр класса
+    ex = GetRecords(user_id=message.from_user.id)
+    # Проверяем есть ли пользователь в базе данных
+    records = ex.get_records()
+    # Если есть
+    if records:
+        # Получаем отфарматированный текст с инфомрацией для пользователя
+        msg = format_func3(records, months)
+        # Отправляем пользователю сообщение
+        await message.answer(text=msg)
+    # Если нету
+    else:
+        # Отправляем пользователю сообщение с просьбой ввести свой номер телефона
+        await message.answer(LEXICON_user['not_registered'])
